8406df0b88e9bbb225e0c9c23214de609afe2bf1
Mikhail <mykhail.nakonechnyi@gmail.com>
2019-10-06 09:46:49 +0000

    first commit

---
+++ lesson1/name.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+//#include "iostream"
+//#include "iostream.h" .hpp
+//#include <stdlib.h> ""    = C
+//#include <cstdlib>        = C++
+//#include <string.h>
+
+// using namespace std;
+
+int main(){
+    // cout << "Hello, world!\n";
+    std::cout << "Hello, world!\n";
+    return 0;
+}
