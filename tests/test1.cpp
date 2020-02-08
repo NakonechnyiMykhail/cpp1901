c304e4ec92128d00e57f99ce8d3adf05371b20e4
Mikhail <mykhail.nakonechnyi@gmail.com>
2020-02-08 22:38:32 +0000

    gleb's circle

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,22 +1,31 @@
-#include <cstdlib>
+// Gleb's question
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main(){
     int count = 0, repeats = 0;
+    char phrase[20];
+
+    cout << "enter a phrase: ";
+    // cin.getline(phrase, 20); //v1
+    cin.get(phrase, 20).get();  //v2
+
     cout << "count of lines: ";
     cin>> count;
 
     cout << "number of repeat: ";
     cin >> repeats;
 
+
+
     for(int i = 1; i <= count; i++){
-        for(int j = 1; j <= i; j++){
+        for(int j = 1; j < i; j++){
             cout << " ";
         }
         for(int u = 0; u < repeats; u++){
-            cout << "1 ";
+            cout << phrase;
         }
         cout << endl;
     }
